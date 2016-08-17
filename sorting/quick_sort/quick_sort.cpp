d90956923b91a5dd402bfdd9d58d0dbcb7760dc6
Nguyen Hoang Son <nguyenhoangson@example.com>
2016-08-17 12:52:35 +0800

    Test partition has been passed

--- sorting/quick_sort/quick_sort.cpp
+++ sorting/quick_sort/quick_sort.cpp
@@ -1,8 +1,43 @@
+/*** 
+     Quick sort implementation 
+***/
 #include "quick_sort.h"
 
+/* This will return an index p such that
+   A[k] <= A[p]; for all k;  left <= k < p
+   A[k] >= A[p]; for all k;  p < k <= right
+*/
 int partition(int *A, int left, int right){
   /*TODO: fill in code here*/
-  return 1;
+  
+  /* The main idea is that 
+     we set the last (A[right]) element as pivot 
+     (last or first does not matter), we then maintain 
+     the following loop invariant: 
+     1. A[k] <= pivot; for all k;  left <= k <= i
+     2. A[k] > pivot; for all k;  i < k <= j
+   */
+  int pivot = *(A+right);
+  int i = left - 1;
+  int temp = 0;
+  
+  for(int j = left; j < right; j++){
+    if(A[j] <= pivot){
+      // swap A[i+1] with A[j]
+      temp = *(A+i+1);
+      *(A+i+1) = *(A+j);
+      *(A+j) = temp;
+      
+      i++; 
+    }
+  }
+
+  // swap A[i+1] with A[right]
+  temp = *(A+i+1);
+  *(A+i+1) = *(A+right);
+  *(A+right) = temp;
+  
+  return i+1;
 }
 
 void quick_sort(int *A, int left, int right){
--- sorting/quick_sort/test_quick_sort.cpp
+++ sorting/quick_sort/test_quick_sort.cpp
@@ -2,9 +2,16 @@
 #include "quick_sort.h"
 
 TEST(QuickSort, TestPartition){
-  int A[1] = {1};
+  int A[7] = {3, 7, 8, 5, 4, 9, 6};
+  int B[7] = {3, 7, 8, 5, 4, 9, 6};
+  int C[9] = {1, 8, 9, 6, 5, 4, 2, 3, 7};
+  int D[9] = {1, 8, 9, 6, 5, 4, 2, 3, 7};
   
-  EXPECT_EQ(1, partition(A, 0, 0));
+  EXPECT_EQ(3, partition(A, 0, 6));
+  EXPECT_EQ(5, partition(B, 2, 5));
+  EXPECT_EQ(6, partition(C, 0, 8));
+  EXPECT_EQ(4, partition(D, 3, 7));
+  EXPECT_EQ(6, partition(D, 6, 6));
 }
 
 TEST(QuickSort, TestQuickSort){
